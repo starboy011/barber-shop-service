178f082f77938f20b670dab76a8242b667538cf6
starboy011 <rishavofficial294@gmail.com>
2024-06-17 14:01:25 +0100

    changed port

--- main.go
+++ main.go
@@ -7,8 +7,8 @@ import (
 
 func main() {
 
-	fmt.Println("Starting server on :8080")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	fmt.Println("Starting server on :8081")
+	if err := http.ListenAndServe(":8081", nil); err != nil {
 		fmt.Printf("Error starting server: %s\n", err)
 	}
 }
