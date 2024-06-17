5049d9d55729168438dc9d6df1ce522fb2f6d481
starboy011 <rishavofficial294@gmail.com>
2024-06-17 13:55:26 +0100

    added go.mod

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/starboy011/barber-shop-service
+
+go 1.22.3
---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+
+	fmt.Println("Starting server on :8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		fmt.Printf("Error starting server: %s\n", err)
+	}
+}
